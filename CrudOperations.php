dd2cc582cd1d9ae6c1df3480335249febe486fbe
sushil <65329897+sushilthelearner@users.noreply.github.com>
2021-07-02 23:37:44 +0530

    Update CrudOperations.php

--- CrudOperations.php
+++ CrudOperations.php
@@ -4,9 +4,14 @@ class CrudOperations
     public function saveData($connection,$name,$email,$contact,$editId,$country,$city,$state,$course,$countryVal,$stateVal,$cityVal)
     {
         if ($editId == "") {
-            $query = "INSERT INTO crud_application SET name='$name',email='$email',contact='$contact',country='$country',state='$state',course='$course',city='$city',cval='$countryVal',stateVal='$stateVal',cityVal='$cityVal'";
+            $query = "INSERT INTO crud_application SET name='$name',email='$email',contact='$contact',
+            country='$country', state='$state',course='$course',city='$city',cval='$countryVal',
+            stateVal='$stateVal',cityVal='$cityVal'";
         }else{
-            $query = "UPDATE crud_application SET name='$name',email='$email',contact='$contact',country='$country',state='$state',city='$city' ,course='$course',cval='$countryVal',stateVal='$stateVal',cityVal='$cityVal' WHERE id='$editId' ";
+            $query = "UPDATE crud_application SET name='$name',email='$email',contact='$contact',
+            country='$country', state='$state',city='$city' ,course='$course',cval='$countryVal',
+            stateVal='$stateVal', cityVal='$cityVal' WHERE id='$editId' ";
+            
         }
         $result = $connection->query($query) or die("Error in saving data".$connection->error);
         return $result;
