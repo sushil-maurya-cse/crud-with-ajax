efdc648724f3ee597045b5528ffd1bc84173cff5
sushil <65329897+sushilthelearner@users.noreply.github.com>
2021-07-02 23:40:28 +0530

    Update CrudOperations.php

--- CrudOperations.php
+++ CrudOperations.php
@@ -2,12 +2,15 @@
 class CrudOperations
 {
     public function saveData($connection,$name,$email,$contact,$editId,$country,$city,$state,$course,$countryVal,$stateVal,$cityVal)
-    {
+    {   
+        // Condition for Checking edit id present or not....
         if ($editId == "") {
+        // If not do the insertion operations    
             $query = "INSERT INTO crud_application SET name='$name',email='$email',contact='$contact',
             country='$country', state='$state',course='$course',city='$city',cval='$countryVal',
             stateVal='$stateVal',cityVal='$cityVal'";
-        }else{
+        } else {
+            // Else do the Deletion operations   
             $query = "UPDATE crud_application SET name='$name',email='$email',contact='$contact',
             country='$country', state='$state',city='$city' ,course='$course',cval='$countryVal',
             stateVal='$stateVal', cityVal='$cityVal' WHERE id='$editId' ";
@@ -19,6 +22,7 @@ class CrudOperations
 
     public function getAllData($connection)
     {
+        // Get the Data from the Database
         $query = "SELECT * FROM crud_application";
         $result = $connection->query($query) or die("Error in getting data".$connection->error);
         return $result;
